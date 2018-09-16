f481f91352aba6d9ae446845cec14f153ad39579
André Xavier <andre.macedo@aluno.positivo.edu.br>
2018-09-16 02:06:13 -0300

    first commit

---
+++ MobikeApp/MobikeApp/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Prism;
+using Prism.Ioc;
+using MobikeApp.ViewModels;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using MobikeApp.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Prism.DryIoc;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace MobikeApp
+{
+    public partial class App : PrismApplication
+    {
+        /* 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            AppCenter.Start("android=c181f1ac-ea8d-48eb-baf6-4fc3dfa9e57d;" + 
+                "uwp={Your UWP App secret here};" + 
+                "ios={Your iOS App secret here}", 
+                typeof(Analytics), typeof(Crashes));
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage>();
+        }
+    }
+}
