c5de01c80d3b7e786754799f4fb6170001ff4a81
André Xavier <andre.macedo@aluno.positivo.edu.br>
2018-09-16 19:29:58 -0300

    credential services

---
+++ MobikeApp/MobikeApp.Android/Services/CredentialsService.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Xamarin.Auth;
+using Xamarin.Forms;
+using MobikeApp.Droid.Services;
+//using MobikeBusinessObject.IServices;
+using Android.App;
+
+[assembly: Dependency(typeof(CredentialsService))]
+namespace MobikeApp.Droid.Services
+{
+    public class CredentialsService //: ICredentialsService
+    {
+        public string Code => throw new System.NotImplementedException();
+
+        public string UserName => throw new System.NotImplementedException();
+
+        public string Email => throw new System.NotImplementedException();
+
+        public string Token => throw new System.NotImplementedException();
+
+        public void DeleteCredentials()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DoCredentialsExist()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveCredentials(string codigo, string userName, string email, string token)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
