4aac7b7e359217e68e4c64850fd48da57456733e
cleber batista <cleberbatista@MacBook-Pro-de-cleber.local>
2018-09-17 20:57:49 -0300

    mac

---
+++ MobikeApp/MobikeApp/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mobike.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Valor { get; set; }
+    }
+}
---
+++ MobikeApp/MobikeApp/ViewModels/Produto/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MobikeApp.Mdels;
+
+namespace MobikeApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item)
+        {
+            Title = item.Nome;
+            Item = item;
+        }
+    }
+}
